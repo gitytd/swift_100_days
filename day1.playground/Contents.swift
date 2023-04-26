367237c1f624530be7b96074e254b54ff92c45af
gitytd <demiryahyatayyip@gmail.com>
2023-04-26 20:36:30 +0300

    100 days of Swift day:1

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+var str = "Hello World"
+print(str)
+str = "goodbye"
+print(str)
+
+var age = 38
+var turkeyPopulation = 85_000_000
+
+var str1 = """
+bu şiir
+3 satırdan
+oluşmaktadır
+"""
+print(str1)
+
+var str2 = """
+bu şiir\
+3 satırdan\
+oluşmaktadır
+"""
+print(str2)
+
+var pi = 3.14
+var awesome = true
+
+print(awesome)
+
+var score = 85
+var str3 = "Your score was \(score)"
+var result = "your results are here: \(str3)"
+print(result)
+
+//constant and var difference
+
+//type annotations
+
+
